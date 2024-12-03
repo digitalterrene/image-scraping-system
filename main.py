6ee5d002a5a6ca7a778b481b7bc3e0cc7a3b2f6d
digitalterrene <110802979+digitalterrene@users.noreply.github.com>
2024-12-03 21:50:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # main.py (Root folder)
-
 from fastapi import FastAPI, APIRouter
+from mangum import Mangum  # Required for serverless environments
 from scraper_server.main import app as scraper_app
 from image_search_server.main import app as image_search_app
 
@@ -10,8 +10,6 @@ main_app = FastAPI()
 # Create routers for sub-apps
 scraper_router = APIRouter()
 image_search_router = APIRouter()
-converter_router = APIRouter()
-dynamic_image_router = APIRouter()
 
 # Include all routes from the scraper server under /scraper
 scraper_router.include_router(scraper_app.router, prefix="/scraper")
@@ -21,7 +19,6 @@ image_search_router.include_router(
 # Add the routers to the main app
 main_app.include_router(scraper_router)
 main_app.include_router(image_search_router)
-
 
 @main_app.get("/")
 async def root():
@@ -35,3 +32,6 @@ async def root():
             "/image-search",
         ],
     }
+
+# Create a handler for Vercel
+handler = Mangum(main_app)
